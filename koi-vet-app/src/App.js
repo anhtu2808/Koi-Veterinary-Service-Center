322e7b5e66e8d51422df5701f05162486c0c0b76
Con-Trai-Cung <VH01062023@gmail.com>
2024-09-20 22:52:05 +0700

    update GGM jsx

--- koi-vet-app/src/App.js
+++ koi-vet-app/src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Login from './pages/Login/Login';
 import Payment from './pages/Payment/Payment';
 import FAQ from './pages/FAQ/FAQ';
+import GGM from './pages/GoogleMeet/GGM';
 
 function App() {
   return (
     <div className="App container-fluid">
-      <FAQ/>
+      <GGM/>
     </div>
   );
 }
