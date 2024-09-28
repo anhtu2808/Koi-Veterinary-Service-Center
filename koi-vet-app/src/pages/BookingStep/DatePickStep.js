6ef1fab7ef0268840d8dc2cf12e378aa13882887
Đặng Mai Anh Tú <tudmase182951@fpt.edu.vn>
2024-09-29 00:39:12 +0700

    update datepicker

--- koi-vet-app/src/pages/BookingStep/DatePickStep.js
+++ koi-vet-app/src/pages/BookingStep/DatePickStep.js
@@ -1,86 +1,216 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../components/DatePicker/datepicker.css'
+const response  = {
+  "status": "200",
+  "message": "Get Schedule ID Successfully",
+  "data": [
+      {
+          "day": "2024-10-01",
+          "slots": [
+              {
+                  "startTime": "08:00:00",
+                  "endTime": "10:00:00"
+              },
+              {
+                  "startTime": "13:00:00",
+                  "endTime": "15:00:00"
+              },
+              {
+                  "startTime": "16:30:00",
+                  "endTime": "18:00:00"
+              }
+          ]
+      },
+      {
+          "day": "2024-10-02",
+          "slots": [
+              {
+                  "startTime": "09:30:00",
+                  "endTime": "11:30:00"
+              },
+              {
+                  "startTime": "14:00:00",
+                  "endTime": "16:00:00"
+              }
+          ]
+      },
+      {
+          "day": "2024-10-03",
+          "slots": [
+              {
+                  "startTime": "10:00:00",
+                  "endTime": "12:00:00"
+              },
+              {
+                  "startTime": "15:30:00",
+                  "endTime": "17:30:00"
+              }
+          ]
+      },
+      {
+          "day": "2024-10-04",
+          "slots": [
+              {
+                  "startTime": "08:30:00",
+                  "endTime": "10:30:00"
+              },
+              {
+                  "startTime": "11:00:00",
+                  "endTime": "13:00:00"
+              },
+              {
+                  "startTime": "14:30:00",
+                  "endTime": "16:30:00"
+              }
+          ]
+      },
+      {
+          "day": "2024-10-07",
+          "slots": [
+              {
+                  "startTime": "09:00:00",
+                  "endTime": "11:00:00"
+              },
+              {
+                  "startTime": "13:30:00",
+                  "endTime": "15:30:00"
+              },
+              {
+                  "startTime": "16:00:00",
+                  "endTime": "18:00:00"
+              }
+          ]
+      },
+      {
+          "day": "2024-10-08",
+          "slots": [
+              {
+                  "startTime": "10:30:00",
+                  "endTime": "12:30:00"
+              },
+              {
+                  "startTime": "14:00:00",
+                  "endTime": "16:00:00"
+              }
+          ]
+      },
+      {
+          "day": "2024-10-09",
+          "slots": [
+              {
+                  "startTime": "08:00:00",
+                  "endTime": "10:00:00"
+              },
+              {
+                  "startTime": "11:30:00",
+                  "endTime": "13:30:00"
+              },
+              {
+                  "startTime": "15:00:00",
+                  "endTime": "17:00:00"
+              }
+          ]
+      }
+  ]
+}
+
 const DatePickStep = () => {
+  const [currentDate, setCurrentDate] = useState(new Date())
+  const [selectedDate, setSelectedDate] = useState(null)
+
+  const renderDays = () => {
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth();
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    let firstDayOfMonth = new Date(year, month, 1).getDay();
+    // Điều chỉnh để thứ 2 là ngày đầu tuần
+    firstDayOfMonth = firstDayOfMonth === 0 ? 6 : firstDayOfMonth - 1;
+    
+    const days = [];
+
+    // Thêm tiêu đề cho các ngày trong tuần
+    const weekDays = ['T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'CN'];
+    weekDays.forEach(day => {
+      days.push(<div key={`weekday-${day}`} className="weekday">{day}</div>);
+    });
+
+    // Thêm ô trống cho các ngày trước ngày đầu tiên của tháng
+    for (let i = 0; i < firstDayOfMonth; i++) {
+      days.push(<div key={`empty-${i}`} className="disabled"></div>);
+    }
+
+    // Render các ngày trong tháng
+    for (let day = 1; day <= daysInMonth; day++) {
+      const date = new Date(year, month, day);
+      const isToday = date.toDateString() === new Date().toDateString();
+      const isSelected = selectedDate && date.toDateString() === selectedDate.toDateString();
+      const isDisabled = response.data.some(item => new Date(item.day) === new Date(year, month , day)); //
+      console.log(isDisabled)
+      days.push(
+        <div
+          key={day}
+          className={`day ${isToday ? 'current-day' : ''} ${isSelected ? 'selected' : ''} ${isDisabled ? 'disabled' : ''}`}
+          onClick={() => !isDisabled && handleDateClick(date)}
+        >
+          {day}
+        </div>
+      );
+    }
+
+    return days;
+  }
+
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+    // Xử lý logic khi chọn ngày ở đây
+  }
+
+  const handlePreviousMonth = () => {
+    setCurrentDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth() - 1, 1));
+  }
+
+  const handleNextMonth = () => {
+    setCurrentDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth() + 1, 1));
+  }
+
+  const formatMonth = (date) => {
+    return date.toLocaleString('vi-VN', { month: 'long', year: 'numeric' });
+  }
+
   return (
     <div>
-      
-        <div className="calendar-container">
-          {/* Header với nút điều hướng tháng */}
-          <div className="calendar-header">
-            <button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-arrow-left-square-fill previous-month-btn" viewBox="0 0 16 16">
-                <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1"/>
-              </svg>
-            </button>
-            <h2>Tháng 09, 2024</h2>
-            <button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-arrow-right-square-fill next-month-btn" viewBox="0 0 16 16">
-                <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1"/>
-              </svg>
-            </button>
-          </div>
+      <div className="calendar-container">
+        {/* Header với nút điều hướng tháng */}
+        <div className="calendar-header">
+          <button onClick={handlePreviousMonth}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-arrow-left-square-fill previous-month-btn" viewBox="0 0 16 16">
+              <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1"/>
+            </svg>
+          </button>
+          <h2>{formatMonth(currentDate)}</h2>
+          <button onClick={handleNextMonth}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-arrow-right-square-fill next-month-btn" viewBox="0 0 16 16">
+              <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1"/>
+            </svg>
+          </button>
+        </div>
 
-          {/* Lưới các ngày trong tháng */}
-          <div className="days-grid">
-            {/* Giả lập dữ liệu ngày của tháng */}
-            <div className="day">1</div>
-            <div className="day disabled">2</div> {/* Không có slot trống */}
-            <div className="day">3</div>
-            <div className="day">4</div>
-            <div className="day">5</div>
-            <div className="day disabled">6</div> {/* Không có slot trống */}
-            <div className="day">7</div>
-
-            <div className="day">8</div>
-            <div className="day">9</div>
-            <div className="day">10</div>
-            <div className="day">11</div>
-            <div className="day">12</div>
-            <div className="day">13</div>
-            <div className="day">14</div>
-
-            <div className="day disabled">15</div> {/* Không có slot trống */}
-            <div className="day">16</div>
-            <div className="day">17</div>
-            <div className="day">18</div>
-            <div className="day">19</div>
-            <div className="day">20</div>
-            <div className="day">21</div>
-
-            <div className="day">22</div>
-            <div className="day">23</div>
-            <div className="day">24</div>
-            <div className="day">25</div>
-            <div className="day">26</div>
-            <div className="day">27</div>
-            <div className="day disabled">28</div> {/* Không có slot trống */}
-
-            <div className="day">29</div>
-            <div className="day">30</div>
-          </div>
-          <hr/>
-          <div className="slots text-start">
-            <h3>Danh sách bác sĩ:</h3>
-            <div className="slot">Anh Tú</div>
-            <div className="slot">Phú Khang</div>
-            <div className="slot">Trịnh Nhân</div>
-            <div className="slot">Huy Vũ</div>
-            <div className="slot">Chí Hào</div>
-       
-            <p>Chọn khung giờ để tiếp tục đặt lịch khám bệnh</p>
-          </div>
-          <hr/>
-          {/* Slot giả lập cho ngày 3 */}
-          <div className="slots text-start">
-            <h3>Khung giờ có sẵn cho ngày 03:</h3>
-            <div className="slot">08:00- 9:30</div>
-            <div className="slot">09:30-10:00</div>
-            <div className="slot">09:30-10:00</div>
-            <p>Chọn khung giờ để tiếp tục đặt lịch khám bệnh</p>
-          </div>
+        {/* Lưới các ngày trong tháng */}
+        <div className="days-grid">
+          {renderDays()}
+        </div>
+        
+        <hr />
+        {/* Slot giả lập cho ngày 3 */}
+        <div className="slots text-start">
+          <h3>Khung giờ có sẵn cho ngày 03:</h3>
+          <div className="slot">08:00- 9:30</div>
+          <div className="slot">09:30-10:00</div>
+          <div className="slot">09:30-10:00</div>
+          <p>Chọn khung giờ để tiếp tục đặt lịch khám bệnh</p>
         </div>
       </div>
-    
+    </div>
   )
 }
 
--- koi-vet-app/src/pages/BookingStep/ServiceStep.js
+++ koi-vet-app/src/pages/BookingStep/ServiceStep.js
@@ -2,45 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { fetchServiceByTypeAPI } from '../../apis'
 import Service from '../../components/Service/Service'
-// Fake data
-const fakeServices = [
-    {
-        serviceId: 1,
-        serviceName: "Screen Repair",
-        description: "Fix cracked or damaged screens",
-        image: "https://example.com/screen-repair.jpg"
-    },
-    {
-        serviceId: 2,
-        serviceName: "Battery Replacement",
-        description: "Replace old or faulty batteries",
-        image: "https://example.com/battery-replacement.jpg"
-    },
-    {
-        serviceId: 3,
-        serviceName: "Software Update",
-        description: "Update your device's operating system",
-        image: "https://example.com/software-update.jpg"
-    },
-    {
-        serviceId: 4,
-        serviceName: "Data Recovery",
-        description: "Recover lost or deleted data",
-        image: "https://example.com/data-recovery.jpg"
-    },
-    {
-        serviceId: 5,
-        serviceName: "Camera Repair",
-        description: "Fix faulty cameras or lenses",
-        image: "https://example.com/camera-repair.jpg"
-    },
-    {
-        serviceId: 6,
-        serviceName: "Water Damage Repair",
-        description: "Repair water-damaged devices",
-        image: "https://example.com/water-damage-repair.jpg"
-    }
-];
+
 export const ServiceStep = () => {
     const type = useSelector(state => state?.booking?.bookingData?.type)
     const [services, setServices] = useState([]);
@@ -57,16 +19,13 @@ export const ServiceStep = () => {
         const fetchServiceByType = async () => {
 
             const response = await fetchServiceByTypeAPI("mobile");
-
+            if(response.status === 200){
+                setServices(response.data)
+            }
         }
         fetchServiceByType();
     }, [])
-    useEffect(() => {
-        // Simulating API call with setTimeout
-        setTimeout(() => {
-            setServices(fakeServices);
-        }, 500);
-    }, [])
+   
 
     return (
         <>
