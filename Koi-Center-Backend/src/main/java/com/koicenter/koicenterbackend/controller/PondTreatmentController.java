e11b159d993c1732fcac04b21b39973589d0cb7a
Trinh Nhan <147188783+trinhhnhann@users.noreply.github.com>
2024-09-29 23:52:12 +0700

    update api create pondtreatment

--- Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/controller/PondTreatmentController.java
+++ Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/controller/PondTreatmentController.java
@@ -2,9 +2,11 @@ package com.koicenter.koicenterbackend.controller;
 
 import com.koicenter.koicenterbackend.model.request.PondTreatmentRequest;
 import com.koicenter.koicenterbackend.model.response.PondTreatmentResponse;
+import com.koicenter.koicenterbackend.model.response.ResponseObject;
 import com.koicenter.koicenterbackend.service.PondService;
 import com.koicenter.koicenterbackend.service.PondTreatmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +21,8 @@ public class PondTreatmentController {
     private PondTreatmentService pondTreatmentService;
 
     @PostMapping("/create")
-    public ResponseEntity<PondTreatmentResponse> createPondTreatment(@RequestBody PondTreatmentRequest pondTreatmentRequest) {
-        PondTreatmentResponse response = pondTreatmentService.createPondTreatment(pondTreatmentRequest);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<ResponseObject> createPondTreatment(@RequestBody PondTreatmentRequest pondTreatmentRequest) {
+        PondTreatmentResponse pondTreatmentResponse = pondTreatmentService.createPondTreatment(pondTreatmentRequest);
+        return ResponseObject.APIRepsonse(200, "create successfully!", HttpStatus.CREATED, pondTreatmentResponse);
     }
 }
