1c5969c3d920e642bd50815c6992814d554ee8ae
Lê Huy Vũ <vulhse182602@fpt.edu.vn>
2024-09-29 14:21:53 +0700

    Update logout hey my babe

--- Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/repository/LoggedOutTokenRepository.java
+++ Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/repository/LoggedOutTokenRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface LoggedOutTokenRepository extends JpaRepository<LoggedOutToken, Long> {
-    Optional<LoggedOutToken> findByToken(String token);
+    LoggedOutToken findByToken(String token);
 }
