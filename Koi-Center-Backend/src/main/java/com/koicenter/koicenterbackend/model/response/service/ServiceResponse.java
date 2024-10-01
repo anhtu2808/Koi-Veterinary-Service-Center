9369752b34f1182ec4fc0430e5156eeb09253fdb
Trinh Nhan <147188783+trinhhnhann@users.noreply.github.com>
2024-10-01 16:14:51 +0700

    update response n param koi/pond

---
+++ Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/model/response/service/ServiceResponse.java
@@ -0,0 +1,24 @@
+package com.koicenter.koicenterbackend.model.response.service;
+
+import com.koicenter.koicenterbackend.model.enums.ServiceType;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class ServiceResponse {
+    String serviceId;
+    String serviceName;
+    String description;
+    float basePrice;
+    float pondPrice;
+    float koiPrice;
+    ServiceType serviceFor;
+    String image;
+}
--- Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/service/ServiceService.java
+++ Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/service/ServiceService.java
@@ -3,29 +3,58 @@ package com.koicenter.koicenterbackend.service;
 import com.koicenter.koicenterbackend.exception.AppException;
 import com.koicenter.koicenterbackend.exception.ErrorCode;
 import com.koicenter.koicenterbackend.model.enums.ServiceType;
+import com.koicenter.koicenterbackend.model.response.service.ServiceResponse;
 import com.koicenter.koicenterbackend.repository.ServicesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ServiceService {
     @Autowired
     ServicesRepository servicesRepository;
 
-    public List<com.koicenter.koicenterbackend.model.entity.Service> getAllService(){
-          return servicesRepository.findAll();
+    public List<ServiceResponse> getAllService(){
+        List<com.koicenter.koicenterbackend.model.entity.Service> services = servicesRepository.findAll();
+        List<ServiceResponse> responseList = new ArrayList<>();
+        for (com.koicenter.koicenterbackend.model.entity.Service service : services) {
+            ServiceResponse response = new ServiceResponse();
+            response.setServiceId(service.getServiceId());
+            response.setServiceName(service.getServiceName());
+            response.setDescription(service.getDescription());
+            response.setBasePrice(service.getBasePrice());
+            response.setPondPrice(service.getPondPrice());
+            response.setKoiPrice(service.getKoiPrice());
+            response.setServiceFor(service.getServiceFor());
+            response.setImage(service.getImage());
+            responseList.add(response);
+        }
+          return responseList;
     }
 
 //    public List<com.koicenter.koicenterbackend.model.entity.Service> getAllServiceByType(){
 //        return servicesRepository.findByServiceFor(ServiceType.ONLINE);
 //    }
 
-    public com.koicenter.koicenterbackend.model.entity.Service getServiceById(String id){
-        return servicesRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.SERVICE_NOT_EXITS.getCode(), ErrorCode.SERVICE_NOT_EXITS.getMessage(),HttpStatus.NOT_FOUND));
+    public ServiceResponse getServiceById(String id){
+        com.koicenter.koicenterbackend.model.entity.Service service = servicesRepository.findById(id).orElseThrow(() ->
+                new AppException(ErrorCode.SERVICE_NOT_EXITS.getCode(),
+                        ErrorCode.SERVICE_NOT_EXITS.getMessage(),HttpStatus.NOT_FOUND));
+        ServiceResponse response = new ServiceResponse();
+        response.setServiceId(service.getServiceId());
+        response.setServiceName(service.getServiceName());
+        response.setDescription(service.getDescription());
+        response.setBasePrice(service.getBasePrice());
+        response.setPondPrice(service.getPondPrice());
+        response.setKoiPrice(service.getKoiPrice());
+        response.setServiceFor(service.getServiceFor());
+        response.setImage(service.getImage());
+        return response;
     }
     public List<com.koicenter.koicenterbackend.model.entity.Service> getServiceFor(String servicefor){
         List<com.koicenter.koicenterbackend.model.entity.Service> services = new ArrayList<>();
