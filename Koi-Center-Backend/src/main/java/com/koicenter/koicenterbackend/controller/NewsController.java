e93aec56760c12150d3ca39053d02b9fb8c1f5c8
Lê Huy Vũ <vulhse182602@fpt.edu.vn>
2024-10-02 21:38:17 +0700

    Get all news

---
+++ Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/controller/NewsController.java
@@ -0,0 +1,27 @@
+package com.koicenter.koicenterbackend.controller;
+
+import com.koicenter.koicenterbackend.model.entity.News;
+import com.koicenter.koicenterbackend.model.response.ResponseObject;
+import com.koicenter.koicenterbackend.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/news")
+public class NewsController {
+    @Autowired
+    NewsService newsService;
+
+    @GetMapping
+    public ResponseEntity<ResponseObject> getAllNews() {
+        List<News> newsList = newsService.getAllNews();
+        if(newsList.isEmpty()){
+            return ResponseObject.APIRepsonse(404, "No content", HttpStatus.NOT_FOUND,null);
+        }
+        return ResponseObject.APIRepsonse(200, "Fetched all news successfully", HttpStatus.OK, newsList);
+    }
+}
---
+++ Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/service/NewsService.java
@@ -0,0 +1,21 @@
+package com.koicenter.koicenterbackend.service;
+
+import aj.org.objectweb.asm.commons.Remapper;
+import com.koicenter.koicenterbackend.model.entity.News;
+import com.koicenter.koicenterbackend.repository.NewsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NewsService {
+
+    @Autowired
+    NewsRepository newsRepository;
+    public List<News> getAllNews() {
+        return newsRepository.findAll();
+    }
+
+
+}
