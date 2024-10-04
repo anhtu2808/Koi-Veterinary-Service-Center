8cfc4abf414342e3dafacc0575ba37e08e870166
TranPhuKhag <167117602+TranPhuKhag@users.noreply.github.com>
2024-10-04 14:54:14 +0700

    API getKoiByAppointmentId (table Koi_Treament )

--- Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/configuration/SecurityConfig.java
+++ Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/configuration/SecurityConfig.java
@@ -98,6 +98,9 @@ public class SecurityConfig {
                             .requestMatchers(HttpMethod.POST, "/api/v1/treatments/ponds").permitAll()
                             .requestMatchers(HttpMethod.POST, "/api/v1/treatments/kois").permitAll()
                             .requestMatchers(HttpMethod.GET, "/api/v1/treatments/**").permitAll()
+                            .requestMatchers(HttpMethod.GET, "/api/v1/treatments/ponds/**").permitAll()
+                            .requestMatchers(HttpMethod.GET, "/api/v1/treatments/kois/**").permitAll()
+
 
                             .requestMatchers(HttpMethod.GET, "/api/v1/prescriptions").permitAll()
                             .requestMatchers(HttpMethod.POST, "/api/v1/prescriptions").permitAll()
--- Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/controller/TreatmentController.java
+++ Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/controller/TreatmentController.java
@@ -37,9 +37,18 @@ public class TreatmentController {
         KoiTreatmentResponse koiTreatmentResponse = koiTreatmentService.createKoiTreatment(koiTreatmentRequest);
         return ResponseObject.APIRepsonse(200, "create successfully!", HttpStatus.CREATED, koiTreatmentResponse);
     }
-    @GetMapping("/{appointmentId}")
+    @GetMapping("/ponds/{appointmentId}")
     public ResponseEntity<ResponseObject> getPondByAppointmentId(@PathVariable String appointmentId) {
         List<PondTreatmentResponse> list =  pondTreatmentService.getPondByAppointmentId(appointmentId);
+        if(!list.isEmpty()){
+            return ResponseObject.APIRepsonse(200, "Pond found successfully By Appointment ID ", HttpStatus.OK, list);
+        }else{
+            return ResponseObject.APIRepsonse(404, "Appointment not found", HttpStatus.NOT_FOUND,"");
+        }
+    }
+    @GetMapping("/kois/{appointmentId}")
+    public ResponseEntity<ResponseObject> getKoiByAppointmentId(@PathVariable String appointmentId) {
+        List<KoiTreatmentResponse> list =  koiTreatmentService.getKoiByAppointmentId(appointmentId);
         if(!list.isEmpty()){
             return ResponseObject.APIRepsonse(200, "Pond found successfully By Appointment ID ", HttpStatus.OK, list);
         }else{
