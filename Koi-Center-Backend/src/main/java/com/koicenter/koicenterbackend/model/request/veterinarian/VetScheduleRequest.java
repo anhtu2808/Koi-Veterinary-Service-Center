3f806176ee9dd0406ce08b5740bf849be5385f12
Đặng Mai Anh Tú <tudmase182951@fpt.edu.vn>
2024-10-04 14:29:15 +0700

    Update VetScheduleRequest.java

--- Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/model/request/veterinarian/VetScheduleRequest.java
+++ Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/model/request/veterinarian/VetScheduleRequest.java
@@ -5,13 +5,16 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Data
 public class VetScheduleRequest {
     String vet_id ;
     String schedule_id ;
     AppointmentType appointmentType;
-    LocalTime  startTime ;
+    LocalTime startTime ;
     LocalTime endTime ;
     String serviceId ;
     LocalDate date;
