ed19e9014cfdb79be35047ba9facd05e298fe98d
Đặng Mai Anh Tú <tudmase182951@fpt.edu.vn>
2024-10-06 17:52:26 +0700

    hoàn tất treatment cá

--- Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/mapper/koi/KoiTreatmentMapper.java
+++ Koi-Center-Backend/src/main/java/com/koicenter/koicenterbackend/mapper/koi/KoiTreatmentMapper.java
@@ -2,6 +2,7 @@ package com.koicenter.koicenterbackend.mapper.koi;
 
 import com.koicenter.koicenterbackend.model.entity.KoiTreatment;
 import com.koicenter.koicenterbackend.model.entity.PondTreatment;
+import com.koicenter.koicenterbackend.model.request.koi.KoiTreatmentRequest;
 import com.koicenter.koicenterbackend.model.response.koi.KoiTreatmentResponse;
 import com.koicenter.koicenterbackend.model.response.pond.PondTreatmentResponse;
 import org.mapstruct.Mapper;
@@ -14,5 +15,4 @@ public interface KoiTreatmentMapper {
     @Mapping(source = "koiTreatment.koi.koiId", target = "koiId")
     @Mapping(source = "koiTreatment.prescription.id",target = "prescription_id")
     KoiTreatmentResponse toKoiTreatmentResponse(KoiTreatment koiTreatment);
-
 }
--- koi-vet-app/src/components/Treatment/Treatment.jsx
+++ koi-vet-app/src/components/Treatment/Treatment.jsx
@@ -3,42 +3,41 @@ import { updateKoiTreatmentAPI, updatePondTreatmentAPI } from '../../apis'
 import { toast } from 'react-toastify'
 
 const Treatment = (props) => {
-    const [healthIssue, setHealthIssue] = useState(props.healthIssue)
-    const [treatment, setTreatment] = useState(props.treatment)
+    const [healthIssue, setHealthIssue] = useState(props.healthIssue || "")
+    const [treatment, setTreatment] = useState(props.treatment || "")
     const handleSubmitTreatment = (e) => {
         e.preventDefault();
         let response;
-        if(props.isKoi){
+        if (props.isKoi) {
             const updateKoiTreatment = async () => {
                 response = await updateKoiTreatmentAPI({
                     healthIssue: healthIssue,
-                treatment: treatment,
-                koiId: props.koiId,
-                appointmentId: props.appointmentId
-            })
-            if(response.status === 200){
-                toast.success(response.data.message)
-                props.onUpdate()
-              }
+                    treatment: treatment,
+                    koiTreatmentId: props.treatmentId
+
+                })
+                if (response.status === 200) {
+                    toast.success(response.data.message)
+                    props.onUpdate()
+                }
             }
             updateKoiTreatment()
-        }else if(props.isPond){
+        } else if (props.isPond) {
             const updatePondTreatment = async () => {
                 response = await updatePondTreatmentAPI({
                     healthIssue: healthIssue,
-                treatment: treatment,
-                pondId: props.pondId,
-                appointmentId: props.appointmentId
-                
-            })
-            if(response.status === 200){
-                toast.success(response.data.message)
-                props.onUpdate()
-              }
+                    treatment: treatment,
+                    pondId: props.pondId,
+                    appointmentId: props.appointmentId,
+                })
+                if (response.status === 200) {
+                    toast.success(response.data.message)
+                    props.onUpdate()
+                }
             }
             updatePondTreatment()
         }
-       
+
     }
 
     return (
@@ -52,7 +51,7 @@ const Treatment = (props) => {
                     </div>
                     <div className="form-group">
                         <label htmlFor="treatment">Treatment</label>
-                        <input type="text" className="form-control" id="treatment" name="treatment"value={treatment} onChange={(e) => setTreatment(e.target.value)}  />
+                        <input type="text" className="form-control" id="treatment" name="treatment" value={treatment} onChange={(e) => setTreatment(e.target.value)} />
                     </div>
                 </div>
 
